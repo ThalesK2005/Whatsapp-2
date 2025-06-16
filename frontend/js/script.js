a1115cb5161001be109ba2893d9e4cf5dbbf660a
Thales Kuroishi <thales292005@gmail.com>
2025-06-16 12:56:27 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("https://whatsapp-2-bc5f.onrender.com")
     websocket.onmessage = processMessage
 }
 
